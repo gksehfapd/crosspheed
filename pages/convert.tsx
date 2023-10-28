66fd983f95ed8281ba0d3dcacce909bd082e2c67
gksehfapd <1ehfapd@gmail.com>
2023-10-28 21:56:00 +0900

    style: Convert

--- pages/convert.tsx
+++ pages/convert.tsx
@@ -18,32 +18,45 @@ export default function Convert() {
 	}
 
 	return (
-		<div className="flex flex-col">
-			<span>{flip ? 'kg to lb' : 'lb to kg'}</span>
-			<span>
-				<input
-					type="number"
-					placeholder="lb"
-					value={flip ? Math.round(lb / 0.453592) : lb}
-					onChange={convertKg}
-					id="lb"
-					disabled={flip}
-				/>
-				<label htmlFor="lb">lb</label>
-			</span>
-			<span>
-				<input
-					type="number"
-					placeholder="kg"
-					value={flip ? lb : Math.round(lb * 4.53592) / 10}
-					onChange={convertKg}
-					id="kg"
-					disabled={!flip}
-				/>
-				<label htmlFor="kg">kg</label>
-			</span>
-			<button onClick={reset}>Reset</button>
-			<button onClick={onFlip}>FLIP</button>
+		<div className="flex flex-col items-center text-lg bg-slate-100 p-3 rounded-xl space-y-2">
+			<div className="flex flex-col space-y-2">
+				<span>
+					<input
+						className="w-12 px-1"
+						type="number"
+						placeholder="lb"
+						value={flip ? Math.round(lb / 0.453592) : lb}
+						onChange={convertKg}
+						id="lb"
+						disabled={flip}
+					/>
+					<label htmlFor="lb"> lb</label>
+				</span>
+				<span>
+					<input
+						className="w-12 px-1"
+						type="number"
+						placeholder="kg"
+						value={flip ? lb : Math.round(lb * 4.53592) / 10}
+						onChange={convertKg}
+						id="kg"
+						disabled={!flip}
+					/>
+					<label htmlFor="kg"> kg</label>
+				</span>
+			</div>
+			<button
+				onClick={reset}
+				className="px-2 py-0.5 bg-slate-200 rounded-lg hover:bg-slate-300 active:bg-slate-400"
+			>
+				Reset
+			</button>
+			<button
+				onClick={onFlip}
+				className="px-2 py-0.5 bg-slate-200 rounded-lg hover:bg-slate-300 active:bg-slate-400"
+			>
+				lb {flip ? <span>&larr;</span> : <span>&rarr;</span>} kg
+			</button>
 		</div>
 	)
 }
