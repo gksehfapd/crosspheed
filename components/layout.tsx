d2a116b9799c85f1af6d48483e32a6aec46cb701
gksehfapd <1ehfapd@gmail.com>
2023-11-01 17:04:33 +0900

    style: layout title font-bold

--- components/layout.tsx
+++ components/layout.tsx
@@ -18,7 +18,7 @@ export default function Layout({ title, canGoBack, hasTabBar, children, search }
 	}
 	return (
 		<div>
-			<div className="bg-white w-full h-12 max-w-xl justify-center text-lg px-10 font-medium fixed text-gray-800 border-b top-0 flex items-center">
+			<div className="bg-white w-full h-12 max-w-xl justify-center text-lg px-10 font-bold fixed text-gray-800 border-b top-0 flex items-center">
 				{title ? (
 					<span
 						className={
