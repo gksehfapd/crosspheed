0240278264a3de6c57b2141e7ecfc9445f0f72d5
gksehfapd <1ehfapd@gmail.com>
2023-11-06 22:48:58 +0900

    feat: TABATA Timer

---
+++ pages/timers/tabata.tsx
@@ -0,0 +1,79 @@
+import Countdown from '@/components/countdown'
+import Layout from '@/components/layout'
+import { countDownState } from '@/libs/client/atoms'
+import { cls } from '@/libs/client/utils'
+
+import { useEffect, useState } from 'react'
+import { useRecoilValue } from 'recoil'
+
+export default function Tavata() {
+	const isRunning = useRecoilValue(countDownState)
+	const [isWork, setIsWork] = useState(true)
+
+	const [countWorkSec, setCountWorkSec] = useState(20)
+	const [countRestSec, setCountRestSec] = useState(10)
+	const [countRound, setCountRound] = useState(1)
+
+	useEffect(() => {
+		if (isRunning) {
+			if (countWorkSec > 0) {
+				const timer = setInterval(() => {
+					setCountWorkSec((prev) => prev - 1)
+				}, 1000)
+
+				return () => {
+					clearInterval(timer)
+					console.log(countWorkSec)
+				}
+			} else if (countWorkSec <= 0) {
+				setIsWork(false)
+
+				if (countRestSec > 0) {
+					const timer = setInterval(() => {
+						setCountRestSec((prev) => prev - 1)
+					}, 1000)
+
+					return () => {
+						clearInterval(timer)
+					}
+				} else if (countRestSec <= 0) {
+					setCountRound((prev) => prev + 1)
+					setCountWorkSec(20)
+					setCountRestSec(10)
+					setIsWork(true)
+				}
+			}
+		} else if (!isRunning) {
+			//여기 먼저 실행 됨
+			setCountWorkSec(20)
+			setCountRestSec(10)
+			setCountRound(1)
+			setIsWork(true)
+		}
+	}, [countWorkSec, countRestSec, countRound, isRunning])
+
+	return (
+		<Layout title="TABATA" canGoBack isCenter>
+			<Countdown>
+				<div className="flex flex-col">
+					<div className="flex relative">
+						<span className="w-20 h-20 flex justify-center items-center text-blue-500">
+							{String(countRound).padStart(2, '0')}
+						</span>
+						<span className="w-20 h-20 flex justify-center items-center">00</span>
+						<span className="w-12 h-20 flex justify-center items-center">:</span>
+						<span className="w-20 h-20 flex justify-center items-center">
+							{isWork
+								? String(countWorkSec).padStart(2, '0')
+								: String(countRestSec).padStart(2, '0')}
+						</span>
+						<div className="text-3xl absolute -bottom-8 right-4 text-green-500 space-x-3">
+							<span className={cls(isWork ? '' : 'opacity-0')}>H1</span>
+							<span className={cls(isWork ? 'opacity-0' : '')}>H2</span>
+						</div>
+					</div>
+				</div>
+			</Countdown>
+		</Layout>
+	)
+}
