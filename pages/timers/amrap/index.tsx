fb3f8655b60fc82d3d0f71562bfe67350786ba13
gksehfapd <1ehfapd@gmail.com>
2023-11-08 18:56:46 +0900

    fix: fortime defaultcheck

--- pages/timers/amrap/index.tsx
+++ pages/timers/amrap/index.tsx
@@ -95,6 +95,7 @@ export default function ForTime() {
 										value="up"
 										id="field-up"
 										className="appearance-none"
+										defaultChecked
 									/>
 									UP
 								</div>
