1ff17b7a6794d3678ec9f46d57b23e48da76ac1c
gksehfapd <1ehfapd@gmail.com>
2023-11-24 21:43:20 +0900

    feat: Work & Rest

--- pages/timers/worknrest.tsx
+++ pages/timers/worknrest.tsx
@@ -1,64 +1,129 @@
 import Countdown from '@/components/countdown'
 import Layout from '@/components/layout'
 import Padstart from '@/components/padstart'
-import { countDownState, setEmomMinState, setEmomRoundState, setState } from '@/libs/client/atoms'
+import {
+	countDownState,
+	setEmomRoundState,
+	setRestMinState,
+	setRestSecState,
+	setState,
+	setWorkMinState,
+	setWorkRoundState,
+	setWorkSecState
+} from '@/libs/client/atoms'
 import { useEffect, useState } from 'react'
 import { useRecoilValue } from 'recoil'
-import EmomForm from '@/components/emomForm'
 import WorkNRestForm from '@/components/worknrestForm'
+import { cls } from '@/libs/client/utils'
 
 export default function Emom() {
 	const isRunning = useRecoilValue(countDownState)
 	const isSet = useRecoilValue(setState)
 
-	const useEmomMin = useRecoilValue(setEmomMinState)
-	const useRound = useRecoilValue(setEmomRoundState)
+	const useWNRWorkMin = useRecoilValue(setWorkMinState)
+	const useWNRWorkSec = useRecoilValue(setWorkSecState)
+	const useWNRRestMin = useRecoilValue(setRestMinState)
+	const useWNRRestSec = useRecoilValue(setRestSecState)
+	const useRound = useRecoilValue(setWorkRoundState)
 
-	const [countSec, setCountSec] = useState(0)
-	const [countMin, setCountMin] = useState(0)
+	const [countWorkSec, setCountWorkSec] = useState(0)
+	const [countWorkMin, setCountWorkMin] = useState(0)
+	const [countRestSec, setCountRestSec] = useState(0)
+	const [countRestMin, setCountRestMin] = useState(0)
 	const [countRound, setCountRound] = useState(1)
+
+	const [isWork, setIsWork] = useState(true)
 
 	useEffect(() => {
 		if (isRunning) {
 			if (countRound <= useRound) {
-				if (countMin < useEmomMin) {
-					if (countSec < 60) {
-						const timer = setInterval(() => {
-							setCountSec((prev) => prev + 1)
-						}, 1000)
-						return () => {
-							clearInterval(timer)
+				if (isWork) {
+					if (countWorkMin > 0) {
+						if (countWorkSec > 0) {
+							const timer = setInterval(() => {
+								setCountWorkSec((prev) => prev - 1)
+							}, 1000)
+							return () => {
+								clearInterval(timer)
+							}
+						} else if (countWorkSec == 0) {
+							const timer = setInterval(() => {
+								setCountWorkMin((prev) => prev - 1)
+								setCountWorkSec(59)
+							}, 1000)
+							return () => {
+								clearInterval(timer)
+							}
 						}
-					} else if (countSec >= 60) {
-						setCountMin((prev) => prev + 1)
-						setCountSec(0)
-						const timer = setInterval(() => {
-							setCountSec((prev) => prev + 1)
-						}, 1000)
-						return () => {
-							clearInterval(timer)
+					} else if (countWorkMin === 0) {
+						if (countWorkSec > 0) {
+							const timer = setInterval(() => {
+								setCountWorkSec((prev) => prev - 1)
+							}, 1000)
+							return () => {
+								clearInterval(timer)
+							}
+						} else if (countWorkSec == 0) {
+							setIsWork(false)
+							setCountWorkSec(useWNRWorkSec)
+							setCountWorkMin(useWNRWorkMin)
 						}
 					}
-				} else if (countMin >= useEmomMin) {
-					setCountMin(0)
-					setCountSec(0)
-					setCountRound((prev) => prev + 1)
-					const timer = setInterval(() => {
-						setCountSec((prev) => prev + 1)
-					}, 1000)
-					return () => {
-						clearInterval(timer)
+				}
+				if (!isWork) {
+					if (countRestMin > 0) {
+						if (countRestSec > 0) {
+							const timer = setInterval(() => {
+								setCountRestSec((prev) => prev - 1)
+							}, 1000)
+							return () => {
+								clearInterval(timer)
+							}
+						} else if (countRestSec == 0) {
+							const timer = setInterval(() => {
+								setCountRestMin((prev) => prev - 1)
+								setCountRestSec(59)
+							}, 1000)
+							return () => {
+								clearInterval(timer)
+							}
+						}
+					} else if (countRestMin === 0) {
+						if (countRestSec > 0) {
+							const timer = setInterval(() => {
+								setCountRestSec((prev) => prev - 1)
+							}, 1000)
+							return () => {
+								clearInterval(timer)
+							}
+						} else if (countRestSec == 0) {
+							setIsWork(true)
+							setCountRestSec(useWNRRestSec)
+							setCountRestMin(useWNRRestMin)
+							setCountRound((prev) => prev + 1)
+						}
 					}
 				}
 			} else if (countRound > useRound) {
 				console.log('끝')
 			}
 		} else if (!isRunning) {
-			setCountSec(0)
-			setCountMin(0)
-			setCountRound(1)
+			setCountWorkSec(useWNRWorkSec)
+			setCountWorkMin(useWNRWorkMin)
+			setCountRestSec(useWNRRestSec)
+			setCountRestMin(useWNRRestMin)
+			setCountRound(0)
+			setIsWork(true)
+			console.log(
+				'!isRunning after : ',
+				useWNRWorkMin,
+				useWNRWorkSec,
+				useWNRRestMin,
+				useWNRRestSec,
+				countRound
+			)
 		}
-	}, [countSec, countMin, countRound, isRunning])
+	}, [countWorkSec, countWorkMin, countRestSec, countRestMin, countRound, isRunning])
 
 	return (
 		<Layout title="WORK & REST" canGoBack isCenter>
@@ -67,12 +132,15 @@ export default function Emom() {
 					<div className="flex flex-col">
 						<div className="flex relative">
 							<Padstart text={countRound} tailwindCss="text-blue-500" />
-							<Padstart text={countMin} />
+							<Padstart text={isWork ? countWorkMin : countRestMin} />
 							<Padstart text=":" narrow />
-							<Padstart text={countSec} />
-							<div className="text-2xl absolute -bottom-8 right-3 text-green-500 space-x-3 flex flex-col justify-center items-end top-24">
-								<span>E {useEmomMin} MOM</span>
-								<span>{useRound} ROUND</span>
+							<Padstart text={isWork ? countWorkSec : countRestSec} />
+							<div className="text-3xl absolute -bottom-8 right-4 text-green-500 space-x-3">
+								<span className={cls(isWork ? '' : 'opacity-0')}>H1</span>
+								<span className={cls(isWork ? 'opacity-0' : '')}>H2</span>
+							</div>
+							<div className="text-xl absolute -bottom-7 left-6 text-green-500 space-x-3">
+								<span>/ {useRound}</span>
 							</div>
 						</div>
 					</div>
